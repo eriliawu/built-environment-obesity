f41e7d7c0789f3a09f6ab725c1b4f2a477fcb62d
Erilia Wu <eriliawu@users.noreply.github.com>
2018-07-24 16:29:23 -0400

    Update test-file.R

--- test-file.R
+++ test-file.R
@@ -18,7 +18,6 @@ dev.new() #open a new window so that a new plot will show up in the new window a
 #write a function
 #note: if intend to return multiple values from a new function,
 #it has to be assigned to a vector instead of just separate values
-
 fahr_to_celcius <- function(temp) {
   new <- fahr_to_kelvin(temp)
   new <- kelvin_to_celcius(temp)
@@ -33,15 +32,84 @@ fence <- function(original, wrapper="**") {
 
 # loops
 analyze <- function(filename) {
-  #plot teh mean, min, max
+  #plot the mean, min, max
   # read .csv file
   data <- read.csv(file=filename, header=FALSE)
   avg_day_inflammation <- apply(data, 2, mean)
   min_day_inflammation <- apply(data, 2, min)
   max_day_inflammation <- apply(data, 2, max)
-  plot(avg_day_inflammation)
+  plot(avg_day_inflammation, main=paste(filename, "-avg", sep=""))
   dev.new()
-  plot(min_day_inflammation)
+  plot(min_day_inflammation, main=paste(filename, "-min", sep=""))
   dev.new()
-  plot(max_day_inflammation)
+  plot(max_day_inflammation, main=paste(filename, "-max", sep=""))
 }
+
+# writing loops
+print_sentence <- function(sentence) {
+  for (i in sentence) {
+    return(i)
+    print(i)
+  }
+}
+
+print_number <- function(num) {
+  for (i in 0:num) {
+    #return(i)
+    print(i)
+    new_num <- new_num + i
+  }
+  return(new_num)
+}
+
+#see what files and folders are available
+analyze_all <- function(path=".", pattern) {
+  files <- list.files(path=path, pattern=pattern)
+  for (file in files) {
+    pdf(file=paste("/personal/hw1220", filename, ".pdf", sep="")) #
+    analyze(file)
+    dev.off() #so that the plot isnt printed on the screen
+  }
+}
+
+#logical operator
+#boolean
+sign <- function(x) {
+  if (!is.numeric(x)) {
+    stop("error msg: x has to be numeric")
+    #return(NULL)
+  }
+  if (x<0) {
+    return(-1)
+  } else if (x==0) {
+    return(0)
+  } else {
+    return(1)
+  }
+}
+
+plot_dist <- function(data, threshold=10) {
+    if (length(data)<=threshold) {
+      stripchart(data)
+    } else {
+      boxplot(data)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
