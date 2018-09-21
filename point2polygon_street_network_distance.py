451193a63328393c85040d304b6d6f64d9af270f
Erilia Wu <eriliawu@users.noreply.github.com>
2018-09-21 15:00:28 -0400

    Update point2polygon_street_network_distance.py

--- point2polygon_street_network_distance.py
+++ point2polygon_street_network_distance.py
@@ -1,14 +1,22 @@
-### street network distance
-### point to polygon
+# references
+# 1] network analyst module
+# https://resources.arcgis.com/en/help/main/10.2/index.html#//004800000028000000
+# 2] connect anaconda and arcpy
+# https://gis.stackexchange.com/questions/119503/getting-arcpy-to-work-with-anaconda#119507
 
 import arcpy
 from arcpy import env
 
-#set working environment
-env.workspace = "Street_Network.gdb"
+#Check out the Network Analyst extension license
+arcpy.CheckOutExtension("Network")
+env.overwriteOutput = True
+
+# set up work environment
+env.workspace = "H:/Personal/Street_Network.gdb"
 
 # convert parks from polygons to lines
 arcpy.PolygonToLine_management("parks_dissolved", "parksAsLine")
 
 # merge parks and LION file
 arcpy.Merge_management(["Lion_2013/Lion_2013_base", "parksAsLine"], "parkLION_merge")
+
