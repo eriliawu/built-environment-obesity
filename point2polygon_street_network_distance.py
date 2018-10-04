3ed9c82b5fddddfff9b0c85c259559927adb31ba
Erilia Wu <eriliawu@users.noreply.github.com>
2018-10-04 12:27:24 -0400

    Update point2polygon_street_network_distance.py

--- point2polygon_street_network_distance.py
+++ point2polygon_street_network_distance.py
@@ -12,7 +12,7 @@ arcpy.CheckOutExtension("Network")
 env.overwriteOutput = True
 
 # set up work environment
-env.workspace = "H:/Personal/Street_Network.gdb"
+env.workspace = "Street_Network.gdb"
 
 # convert parks from polygons to lines
 arcpy.PolygonToLine_management("parks_dissolved", "parksAsLine")
@@ -34,30 +34,20 @@ arcpy.MakeFeatureLayer_management("parkIntersectPoints", "parkIntersectPoints_la
 arcpy.MakeFeatureLayer_management("parks_dissolved", "parks_dissolved_layer")
 
 radius = [30, 45, 60]
-for distance in radius:  
+for r in radius:  
     select = arcpy.SelectLayerByLocation_management("parkIntersectPoints_layer", "WITHIN_A_DISTANCE", 
-                                                    "parks_dissolved", str(distance)+" feet", "NEW_SELECTION", "NOT_INVERT")
-    arcpy.FeatureClassToFeatureClass_conversion(select, r"H:\Personal\Street_Network.gdb", "parkEntrances_"+str(distance))
+                                                    "parks_dissolved", str(r)+" feet", "NEW_SELECTION", "NOT_INVERT")
+    arcpy.FeatureClassToFeatureClass_conversion(select, r"Street_Network.gdb", "parkEntrances_"+str(r))
 
 # find closest facility
-# load park points as facilities
-# load students as incidents
-# Access the analysis preferences by clicking on the file button next to the drop-down menu the network analyst window 
-# (where you loaded the students and restaurants)
-# Under the Analysis Settings tab, make sure you set Facilities to Find to 1,
-# and Travel from should be Incident to Facility (Radio button)
-# Click Solve in the network analyst toolbar (grid with squiggly line)
-
-# write a loop to cover 5 years
-streetNetwork = r'H:/Personal/Street_Network.gdb'
-facilities = r'H:/Personal/Street_Network.gdb/parkEntrances_30ft'
-years = ["09", "10", "11", "12", "13"]
-
 for t in years:
-    incidents = r'H:/Personal/Street_Network.gdb/s' + t 
-    outGeodatabase = r'H:/Personal/Street_Network.gdb/Lion_20' + t
-    # Run FindClosestFacilities. Choose to find only the closest facility.
-    arcpy.na.FindClosestFacilities(incidents, facilities, "Feet", streetNetwork, outGeodatabase, "Routes", 
-                               "Directions", "nearestPark_30ft" + t, Number_of_Facilities_to_Find=1)
-
+    streetNetwork = r'Street_Network.gdb/Lion_20' + t +'/Lion_20' + t + '_Network'
+    incidents = r'Street_Network.gdb/s' + t
+    outGeodatabase = r'Street_Network.gdb/Lion_20' + t
+    for r in radius:
+        facilities = r'Street_Network.gdb/parkEntrances_' + str(r)
+        route = "nearestPark_" + str(r) + "_" + t
+        # Run FindClosestFacilities. Choose to find only the closest facility
+        arcpy.na.FindClosestFacilities(incidents, facilities, "Feet", streetNetwork, outGeodatabase, route, 
+                               "directions", "closestFacility", Number_of_Facilities_to_Find=1)
 
