4f90592ecbc2ac7c34b3198b1e26425cfbf14612
kvallario <43890258+kvallario@users.noreply.github.com>
2018-10-22 11:40:54 -0400

    Update point2polygon_euclidean.R

--- point2polygon_euclidean.R
+++ point2polygon_euclidean.R
@@ -7,6 +7,7 @@ suppressWarnings(library(sp)) #to prep using gDistance
 suppressWarnings(library(spatstat)) #nncross
 suppressWarnings(library(maptools)) #readShapeLines; read shapefiles
 suppressWarnings(library(rgeos)) #gDistance
+suppressWarnings(library(rgdal)) #readOGR
 
 ### read student data ----
 students <- read.csv("students.csv", stringsAsFactors = FALSE) #clean up the data so it has two columns: x and y
@@ -15,11 +16,12 @@ students <- read.csv("students.csv", stringsAsFactors = FALSE) #clean up the dat
 ### use nncross
 # convert students into ppp object
 coords.students <- ppp(students$x, students$y, 
-               window=owin(xrange=c(0, max(students$x)), 
-                           yrange=c(0, max(students$y))))
+                       window=owin(xrange=c(0, max(students$x)), 
+                                   yrange=c(0, max(students$y))))
 
 # read parks
-parks <- readShapeLines("parks.shp")
+parks <- readOGR(".", "parks")
+parks <- as(parks,"SpatialLines")
 parks <- as.psp(parks)
 
 # find nearest park
@@ -32,7 +34,7 @@ write.csv(students, "dist_to_nearest.csv")
 park.info <- read.csv("list_of_parks.csv", stringsAsFactors = FALSE) #provides unique park ids (var: parknum) and the type of park (var: landuse)
 names(park.info) #clean up so that park.info has 2 vars: parknum and landuse
 
-parks <- readShapeSpatial("parks.shp") #gDistance reads polygons as SpatialPolygons feture class
+parks <- readOGR(".", "parks")
 
 #create functions to count numbr of parks within different parameters
 # buffers: 40, 264, 660, 1320, 2640 ft
@@ -40,33 +42,35 @@ parks <- readShapeSpatial("parks.shp") #gDistance reads polygons as SpatialPolyg
 # how wide should a street be: 
 # http://plannersweb.com/2013/09/wide-neighborhood-street-part-1/
 sum40 <- function(x) { #park is right across the street
-      num <- sum(x<40)
-      return(num)
+  num <- sum(x<40)
+  return(num)
 }
 
 sum264 <- function(x) { #1 city block
-      num <- sum(x<264)
-      return(num)
+  num <- sum(x<264)
+  return(num)
 }
 
 sum660 <- function(x) { #1/8 of a mile
-      num <- sum(x<660)
-      return(num)
+  num <- sum(x<660)
+  return(num)
 }
 
 sum1320 <- function(x) { #1 quarter of a mile
-      num <- sum(x<1320)
-      return(num)
+  num <- sum(x<1320)
+  return(num)
 }
 
 sum2640 <- function(x) { #half a mile
-      num <- sum(x<2640)
-      return(num)
+  num <- sum(x<2640)
+  return(num)
 }
+
+memory.limit(size=56000)
 
 # slice students into smaller groups to calculate student to park distance
 coords.students <- SpatialPoints(students)
-dist <- gDistance(parks, coords.students，byid=TRUE)
+dist <- gDistance(parks, coords.students,byid=TRUE)
 dist <- as.data.frame(dist)
 row <- dim(dist)[1]
 col <- dim(dist)[2]
